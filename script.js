d84419a5514d12ca9be2ff87ee1ce3e864b4c7e1
Mandoralh <92581415+Mandoralh@users.noreply.github.com>
2022-03-11 11:25:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+let helloWorldPopup;
 
-WA.ui.openPopup(targetObject: AgendaPopUp, message: "string", buttons: ButtonDescriptor[]): Popup
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.room.onEnterLayer("myZone").subscribe(() => {
+    WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("myZone").subscribe(() => {
+    helloWorldPopup.close();
+})
